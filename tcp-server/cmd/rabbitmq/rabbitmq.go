cd1d9414c9ab1dc96a6a516c5c0621bf2ed4e180
nenishyi <aworkprogramm@gmail.com>
2026-09-18 14:26:13 +0500

    fix(rabbitmq): stop reconnect cleanly

--- tcp-server/cmd/rabbitmq/rabbitmq.go
+++ tcp-server/cmd/rabbitmq/rabbitmq.go
@@ -40,6 +40,7 @@ func NewRabbitMQ(amqpURL string) (*RabbitMQ, error) {
 
 	/* Initial channels */
 	if err := r.initChannels(); err != nil {
+		_ = conn.Close()
 		return nil, err
 	}
 
@@ -63,7 +64,12 @@ func (r *RabbitMQ) handleReconnect() {
 
 		r.mutex.Lock()
 		conn := r.conn
+		closed := r.closed
 		r.mutex.Unlock()
+
+		if closed {
+			return
+		}
 
 		if conn == nil {
 			time.Sleep(2 * time.Second)
